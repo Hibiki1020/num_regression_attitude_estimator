935a2318f8db179e45e54cdacf975cbc0575c91a
Hibiki Kawai <hibikijitaku@gmail.com>
2021-06-23 14:51:56 +0900

    DNN学習プログラムを実装(未検証)

---
+++ pysrc/common/dataset_mod.py
@@ -0,0 +1,26 @@
+import torch.utils.data as data
+from PIL import Image
+import numpy as np
+
+class Originaldataset(data.Dataset):
+    def __init__(self, data_list, transform, phase):
+        self.data_list = data_list
+        self.transform = transform
+        self.phase = phase
+
+    def __len__(self):
+        return len(self.data_list)
+
+    def __getitem__(self, index):
+        img_path = self.data_list[index][0]
+
+        roll_str = self.data_list[index][4]
+        pitch_str = self.data_list[index][5]
+
+        deg_list = [float(roll_str), float(pitch_str)]
+
+        img_pil = Image.open(img_path)
+        deg_numpy = np.array(deg_list)
+
+        img_trans, deg_trans = self.transform(img_pil, deg_numpy, phase=self.phase)
+        return img_trans, deg_trans
