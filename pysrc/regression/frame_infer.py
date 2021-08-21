601113458f0f96ef42a41551e29548db92d7afe0
Hibiki Kawai <hibikijitaku@gmail.com>
2021-08-21 10:07:48 +0900

    Infer Log Changed

--- pysrc/regression/frame_infer.py
+++ pysrc/regression/frame_infer.py
@@ -183,6 +183,9 @@ class FrameInfer:
             pitch = np.average(pitch_array)
             yaw = np.average(yaw_array)
 
+            roll_var = np.var(roll_array)
+            pitch_var = np.var(pitch_array)
+
             print("Infered Roll:  " + str(roll) +  "[deg]")
             print("GT Roll:       " + str(ground_truth[1]) + "[deg]")
             print("Infered Pitch: " + str(pitch) + "[deg]")
@@ -191,7 +194,7 @@ class FrameInfer:
             #print("GT Yaw:        " + str(ground_truth[3]) + "[deg]")
 
             #image roll, pitch, GTroll, GTpitch
-            tmp_result = [ground_truth[0], roll, pitch, ground_truth[1], ground_truth[2]]
+            tmp_result = [ground_truth[0], roll, pitch, ground_truth[1], ground_truth[2], roll_var, pitch_var]
             result_csv.append(tmp_result)
 
             print("Period [s]: ", time.time() - start_clock)
